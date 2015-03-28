681daa9fa31cc31f6339ffa5eb3ed4ca5334a4be
Michael McCouman Jr. <support@wikibyte.org>
2015-03-28 21:33:03 +0100

    v1.0.2

--- playlist.js
+++ playlist.js
@@ -1,12 +1,3 @@
-/**
- * @file MediaElement Playlist Feature (plugin).
- * @author Andrew Berezovsky <andrew.berezovsky@gmail.com>
- * Twitter handle: duozersk
- * @author Original author: Junaid Qadir Baloch <shekhanzai.baloch@gmail.com>
- * Twitter handle: jeykeu
- * Dual licensed under the MIT or GPL Version 2 licenses.
- */
-
 (function($) {
   $.extend(mejs.MepDefaults, {
     loopText: 'Repeat On/Off',
